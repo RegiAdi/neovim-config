0f872bdc68954d83979040858d415d7469464624
Regi Adi <regiadi.priyanto@gmail.com>
2024-11-10 23:13:28 +0700

    Setup treesitter

--- lua/config/theme/gruvbox.lua
+++ lua/config/theme/gruvbox.lua
@@ -22,25 +22,28 @@ require("gruvbox").setup({
     contrast = "hard", -- can be "hard", "soft" or empty string
     palette_overrides = {},
     overrides = {
-        Function = { link = "GruvboxAqua" },
+        Directory = { link = "GruvboxFg1" },
+
+        Function = { link = "GruvboxYellow" },
         Statement = { link = "GruvboxRed" },
         Operator = { link = "GruvboxAqua" },
         Keyword = { link = "GruvboxOrange" },
         Identifier = { link = "GruvboxBlue" },
-        Special = { link = "GruvboxFg2" },
+        Special = { link = "GruvboxAqua" },
+        Delimiter = { link = "GruvboxFg4" },
 
-        htmlTag = { link = "GruvboxBlue" },
-        htmlEndTag = { link = "GruvboxBlue" },
-        htmlTagName = { link = "GruvboxAqua" },
-        htmlArg = { link = "GruvboxYellow" },
-        htmlSpecialTagName = { link = "GruvboxYellow" },
-
-        javaScriptFunction = { link = "GruvboxAqua" },
-        javaScriptMember = { link = "GruvboxBlue" },
-        javaScriptIdentifier = { link = "GruvboxYellow" },
-
-        typescriptIdentifier = { link = "GruvboxOrange" },
-        typescriptEndColons = { link = "GruvboxGray" },
+        -- htmlTag = { link = "GruvboxBlue" },
+        -- htmlEndTag = { link = "GruvboxBlue" },
+        -- htmlTagName = { link = "GruvboxAqua" },
+        -- htmlArg = { link = "GruvboxYellow" },
+        -- htmlSpecialTagName = { link = "GruvboxYellow" },
+        --
+        -- javaScriptFunction = { link = "GruvboxAqua" },
+        -- javaScriptMember = { link = "GruvboxBlue" },
+        -- javaScriptIdentifier = { link = "GruvboxYellow" },
+        --
+        -- typescriptIdentifier = { link = "GruvboxOrange" },
+        -- typescriptEndColons = { link = "GruvboxGray" },
     },
     dim_inactive = false,
     transparent_mode = false,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	config = function () 
+	config = function ()
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = { 
-				"c", 
-				"lua", 
-				"vim", 
-				"vimdoc", 
-				"query", 
-				"elixir", 
-				"heex", 
-				"javascript", 
-				"html" 
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+                "typescript",
+                "vue",
+				"html",
+                "python"
 			},
 			sync_install = false,
 			highlight = { enable = true },
-			indent = { enable = true },  
+			indent = { enable = true },
 		})
 	end
 }
