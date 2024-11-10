8227a31d2006fd8cde2c602cac49bb7a7d471fd9
Regi Adi <regiadi.priyanto@gmail.com>
2024-11-10 19:19:56 +0700

    Fix vue lsp

---
+++ lua/config/lspconfig.lua
@@ -0,0 +1,29 @@
+require("mason-lspconfig").setup()
+
+-- After setting up mason-lspconfig you may set up servers via lspconfig
+
+-- If you are using mason.nvim, you can get the ts_plugin_path like this
+local mason_registry = require('mason-registry')
+local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
+
+-- local lspconfig = require('lspconfig')
+
+require('lspconfig').ts_ls.setup {
+  init_options = {
+    plugins = {
+      {
+        name = '@vue/typescript-plugin',
+        location = vue_language_server_path,
+        languages = { 'vue' },
+      },
+    },
+  },
+  filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+}
+
+-- No need to set `hybridMode` to `true` as it's the default value
+require('lspconfig').volar.setup {}
+
+require('lspconfig').lua_ls.setup {}
+-- require("lspconfig").rust_analyzer.setup {}
+-- ...
---
+++ lua/config/theme/catppuccin.lua
@@ -0,0 +1,62 @@
+vim.o.background = "dark" -- or "light" for light mode
+
+require("catppuccin").setup({
+    flavour = "auto", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false, -- disables setting the background color.
+    show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+    term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    },
+    no_italic = true, -- Force no italic
+    no_bold = false, -- Force no bold
+    no_underline = true, -- Force no underline
+    styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = {}, -- Change the style of comments
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    },
+    color_overrides = {
+        mocha = {
+            base = "#17191a",
+            mantle = "#17191a",
+            crust = "#17191a",
+        },
+    },
+    custom_highlights = {},
+    default_integrations = true,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        telescope = {
+            enabled = true,
+        }
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+vim.cmd("colorscheme catppuccin")
+
