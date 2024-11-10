f82bca9c26da1392b2578bd9bbe39a238fa2d3bc
Regi Adi <regiadi.priyanto@gmail.com>
2024-11-10 13:30:48 +0700

    Move keymaps and options to separate file

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,27 @@
+function map(mode, shortcut, command)
+  vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+end
+
+function nmap(shortcut, command)
+  map('n', shortcut, command)
+end
+
+function imap(shortcut, command)
+  map('i', shortcut, command)
+end
+
+-- change leader
+vim.g.mapleader = " "
+
+-- map escape to hh
+imap("hh", "<Esc>")
+
+-- map netrw
+nmap("<leader>fe", ":Ex<CR>")
+
+-- map telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
---
+++ lua/config/options.lua
@@ -0,0 +1,32 @@
+-- set encoding
+vim.opt.encoding = "utf-8"
+
+-- a tab size is 4 spaces
+vim.opt.tabstop = 4
+
+-- tab size when inserting/pasting
+vim.opt.softtabstop = 4
+
+-- use multiple of shiftwidth when indenting with '<' and '>'
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- autoindent
+vim.opt.smartindent = true
+
+-- line numbers on
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- line highlight
+vim.opt.cursorline = true
+
+-- don't use backup files
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+-- disable word wrap
+vim.opt.wrap = false
+
+vim.opt.autoread = true
+vim.opt.colorcolumn = "80"
