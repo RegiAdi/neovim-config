9b6e8d32e8def738cc2842cf9a278252b087e891
Regi Adi <regiadi.priyanto@gmail.com>
2025-04-22 21:36:20 +0700

    Configure telescope width

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -1,1 +1,13 @@
-require('telescope').setup({})
+require('telescope').setup({
+    defaults = {
+        layout_strategy = "horizontal",
+            layout_config = {
+            horizontal = {
+                prompt_position = "bottom",
+                width = { padding = 0 },
+                height = { padding = 0 },
+                preview_width = 0.5,
+            },
+        },
+    },
+})
