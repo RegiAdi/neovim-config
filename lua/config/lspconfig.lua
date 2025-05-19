f3fc026c04a3b923ce7fc20f14f934e80532d1aa
Regi Adi <regiadi.priyanto@gmail.com>
2025-05-19 22:10:36 +0700

    Fix ts_ls lsp error

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,12 +1,6 @@
 require("mason-lspconfig").setup()
 
--- After setting up mason-lspconfig you may set up servers via lspconfig
-
--- If you are using mason.nvim, you can get the ts_plugin_path like this
-local mason_registry = require('mason-registry')
--- local vue_language_server_path = mason_registry.get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server'
-
-local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server'
+local vue_language_server_path = vim.fn.expand '$MASON/packages' .. '/vue-language-server'
 
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
